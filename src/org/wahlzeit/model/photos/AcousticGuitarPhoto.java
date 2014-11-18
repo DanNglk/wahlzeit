ff86611ede2460cb7e6e0b621d9d04bf5fb7101e
cdan <dan.nglk@gmail.com>
2014-11-18 16:33:39 +0100

    GuitarPhoto class with subclasses updated.

--- src/org/wahlzeit/model/photos/AcousticGuitarPhoto.java
+++ src/org/wahlzeit/model/photos/AcousticGuitarPhoto.java
@@ -1,9 +1,40 @@
 package org.wahlzeit.model.photos;
 
+import org.wahlzeit.model.PhotoId;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by cdan on 18/11/14.
  */
 public class AcousticGuitarPhoto extends GuitarPhoto {
 
+    /**
+     * @pre
+     * @post
+     * @methodtype constructor
+     */
+    public AcousticGuitarPhoto() {
+    }
 
+
+    /**
+     * @pre
+     * @post
+     * @methodtype constructor
+     */
+    public AcousticGuitarPhoto(PhotoId myId) {
+        super(myId);
+    }
+
+
+    /**
+     * @pre
+     * @post
+     * @methodtype constructor
+     */
+    public AcousticGuitarPhoto(ResultSet rset) throws SQLException {
+        super(rset);
+    }
 }
--- src/org/wahlzeit/model/photos/ElectricGuitarPhoto.java
+++ src/org/wahlzeit/model/photos/ElectricGuitarPhoto.java
@@ -1,5 +1,10 @@
 package org.wahlzeit.model.photos;
 
+import org.wahlzeit.model.PhotoId;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by cdan on 18/11/14.
  */
@@ -7,4 +12,79 @@ public class ElectricGuitarPhoto extends GuitarPhoto {
 
     private int pickups;
 
+
+    /**
+     * @pre
+     * @post
+     * @methodtype constructor
+     */
+    public ElectricGuitarPhoto() {
+        super();
+    }
+
+
+    /**
+     * @pre
+     * @post
+     * @methodtype constructor
+     */
+    public ElectricGuitarPhoto(PhotoId myId) {
+        super(myId);
+    }
+
+
+    /**
+     * @pre
+     * @post
+     * @methodtype constructor
+     */
+    public ElectricGuitarPhoto(ResultSet rset) throws SQLException {
+        super(rset);
+    }
+
+
+    /**
+     * @pre
+     * @post
+     * @methodtype get
+     */
+    public int getPickups() {
+        return pickups;
+    }
+
+
+    /**
+     * @pre pickups > 0
+     * @post correct value set
+     * @methodtype set
+     */
+    public void setPickups(int pickups) {
+        assert pickups > 0;
+        this.pickups = pickups;
+        assert this.pickups == pickups;
+    }
+
+
+    /**
+     * @pre
+     * @post
+     * @methodtype mutation
+     */
+    @Override
+    public void readFrom(ResultSet rset) throws SQLException {
+        super.readFrom(rset);
+        //todo: set attributes
+    }
+
+
+    /**
+     * @pre
+     * @post
+     * @methodtype mutation
+     */
+    @Override
+    public void writeOn(ResultSet rset) throws SQLException {
+        super.writeOn(rset);
+        //todo: write attributes
+    }
 }
